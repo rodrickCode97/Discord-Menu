da68af6826deb8a970d20bf25efc054e2619839f
RodrickCode <rodrickCode@gmail.com>
2022-12-12 20:38:28 -0600

    Discord App Nav bar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Sidebar from './sideBar';
+
+function App() {
+  return (
+    <div className="flex">
+        <Sidebar />
+    </div>
+  );
+}
+
+export default App;
